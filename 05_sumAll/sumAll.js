ae91e9238f2f842ad23e932e4454b1bce9df5cf8
Ira Hudgin <irahudgin@gmail.com>
2022-07-21 15:22:03 -0400

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  let sum = 0;
 
+  if (first < 0 || last < 0) {
+    return "ERROR";
+  } else if (!Number.isInteger(first) || !Number.isInteger(last)) {
+    return "ERROR";
+  } else if (last > first) {
+    for (let i = first; i <= last; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (first > last) {
+    for (let i = last; i <= first; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

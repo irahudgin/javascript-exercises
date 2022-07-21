0eb7ef98e0e25f82da431abdf89a0cf84cb382ca
Ira Hudgin <irahudgin@gmail.com>
2022-07-21 17:38:21 -0400

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  // [°C] = ([°F] − 32) × 5⁄9
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  return +celsius.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  // [°F] = [°C] × 9⁄5 + 32
+  let fahrenheit = celsius * (9 / 5) + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
